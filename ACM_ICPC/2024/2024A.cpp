3b2bc868bccf3aa56ec514b8fc71a0b267b8fb27
KouE10e <KouE10e@users.noreply.github.com>
2025-07-02 20:02:53 +0900

    2024年模擬国内予選A，D問題

--- ACM_ICPC/2024/2024A.cpp
+++ ACM_ICPC/2024/2024A.cpp
@@ -1,24 +1,30 @@
+/*アルゴリズムと工夫点(おやつは 300 円以内/CPU: 0 ms Memory: 3448 KB  Length: 407 B)
+言われたとおりにシミュレーションする．
+最悪計算量は，O(n)<10^3 となり十分高速．
+※問題URL：https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1672&lang=ja
+*/
+//#include<iostream>
+//#include<cassert>
 #include<bits/stdc++.h>
 #define rep(i,n)for(i=0;i<(int)(n);i++)
+#define MAX_MONEY 300
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
 
-int i,j;
+int n;
 
 int main(){
-    while(true){
-        int n;
-        cin >> n;
-        if(n==0) break;
-        vector<int> a(n);
-        rep(i,n) cin >> a[i];
-        int cnt=0;
-        rep(i,n){
-            if(a[i]+cnt<=300){
-                cnt+=a[i];
-            }
+    int i,j;
+    while(1){
+        scanf("%d",&n);
+        if(!n)break;
+        j=0;
+        while(n--){
+            scanf("%d",&i);
+            if(j+i<=MAX_MONEY)j+=i;
         }
-        cout << cnt << endl;
+        printf("%d\n",j);
     }
     return 0;
 }
--- ACM_ICPC/2024/2024D.cpp
+++ ACM_ICPC/2024/2024D.cpp
@@ -1,75 +1,105 @@
+/*アルゴリズムと工夫点(だんごむしではないむし/CPU: 10 ms Memory: 3704 KB  Length: 3287 B)
+拡張BFS的なシミュレーションで解く．
+障害物の座標が小さいため，その最大y座標MAX_Y，最大x座標MAX_Xとし，(MAX_Y+1)*(MAX_X+1)の二次元平面を考える．
+この平面上で虫の移動を，距離dだけ移動し終わるまでシミュレーションする．
+ただし，以下の場合は例外処理を行う．
+・二次元平面の範囲外に出た場合，方向変換は行われないので，解を出力する
+・あるマスに同じ向きから再度訪問する場合，そのマスを始点&終点とするループに入る．ループする分だけ進んだ後は，愚直にシミュレーションする（シミュレーションにより進む距離はMAX_Y*MAX_X以下であるため，問題ない）．
+最悪計算量は，O(n+MAX_Y*MAX_X*4)<10^5 となり十分高速．
+※問題URL：https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1675&lang=ja
+*/
+//#include<iostream>
+//#include<cassert>
 #include<bits/stdc++.h>
 #define rep(i,n)for(i=0;i<(int)(n);i++)
+#define MAX_XY 100
 using namespace std;
 typedef long long ll;
-#define MAX 101
+typedef unsigned long long ull;
 
-struct ins
-{
-    ll x,y,w;
-    ll d;
+struct Point{
+    int y,x,dir;
+
+    Point(int yy=0,int xx=0,int ddir=0):y(yy),x(xx),dir(ddir){}
 };
 
+int n,a,b;
+ll d;
 
 int main(){
-    ll n,i,x,y,a,b;
-    ll d;
-    while(cin>>n,n){
-        cin>>a>>b>>d;
-        vector<vector<bool>> m(MAX,vector<bool>(MAX,0));
-        vector<vector<vector<ll>>> c(MAX,vector<vector<ll>>(MAX,vector<ll>(4,-1)));
-        rep(i,n){
-            cin>>x>>y;
-            m[y][x]=1;
+    int i,j,k;
+    const int NIL=-1,WALL=-2;
+    const int dy[]={-1,0,1,0},MAX_D=4;
+    const int dx[]={0,1,0,-1};
+    while(1){
+        scanf("%d",&n);
+        if(!n)break;
+        scanf("%d%d%lld",&a,&b,&d);
+        vector<vector<vector<int> > > dist(MAX_D,vector<vector<int> >(MAX_XY+1,vector<int>(MAX_XY+1,NIL)));
+        while(n--){
+            scanf("%d%d",&i,&j);
+            rep(k,MAX_D)dist[k][j][i]=WALL;
         }
 
-        queue<ins> q;
-        q.push({a,b,0,0});
-        ins t;
-        int dx[]={1,0,-1,0},dy[]={0,1,0,-1};
-        bool roop=0;
-        while (!q.empty())
-        {
-            t=q.front();
-            q.pop();
-            //printf("%lld:%lld %lld\n",t.d,t.x,t.y);
-            if(t.d==d)break;
-            if(t.x<=100&&t.x>=0&&t.y<=100&&t.y>=0){
-                if(!roop&&c[t.y][t.x][t.w]!=-1){
-                    q.push({t.x,t.y,t.w,
-                    t.d+((d-t.d)/(t.d-c[t.y][t.x][t.w]))*(t.d-c[t.y][t.x][t.w])});
-                    roop=1;
-                    continue;
+        auto bfs=[&](int y,int x){
+            queue<Point> que;
+            Point now(y,x,1);
+            que.push(now);
+            dist[now.dir][now.y][now.x]=0;
+            while(que.size()){
+                now=que.front();que.pop();
+                if(dist[now.dir][y][x]==d){// ループせず終了する場合
+                    //printf("1:");
+                    printf("%d %d\n",now.x,now.y);
+                    return ;
                 }
-                c[t.y][t.x][t.w]=t.d;
-            }
-            x=t.x+dx[t.w],y=t.y+dy[t.w];
-            if(x<0){
-                t.x-=d-t.d;
-                break;
-            }else if(x>100){
-                t.x+=d-t.d;
-                break;
-            }
-            if(y<0){
-                t.y-=d-t.d;
-                break;
-            }else if(y>100){
-                t.y+=d-t.d;
-                break;
-            }
-            if(m[y][x]){
-                while(t.y+dy[(t.w+1)%4]<=100&&t.y+dy[(t.w+1)%4]>=0&&
-                    t.x+dx[(t.w+1)%4]<=100&&t.x+dx[(t.w+1)%4]>=0&&
-                    m[t.y+dy[(t.w+1)%4]][t.x+dx[(t.w+1)%4]]){
-                    t.w=(t.w+1)%4;
+                //printf("(y,x)=(%d,%d) %d\n",now.y,now.x,dist[now.dir][now.y][now.x]);
+                y=now.y+dy[now.dir];
+                x=now.x+dx[now.dir];
+                if(0<=y&&y<=MAX_XY&&0<=x&&x<=MAX_XY){
+                    if(dist[now.dir][y][x]==NIL){
+                        // 行き先が未訪問のマス
+                        dist[now.dir][y][x]=dist[now.dir][now.y][now.x]+1;
+                        que.push(Point(y,x,now.dir));
+                    }else if(dist[now.dir][y][x]==WALL){
+                        // 行き先が障害物の場合
+                        dist[(now.dir+1)%MAX_D][now.y][now.x]=dist[now.dir][now.y][now.x];
+                        que.push(Point(now.y,now.x,(now.dir+1)%MAX_D));
+                    }else{
+                        // 未訪問ではなかった場合
+                        int loop_dist=dist[now.dir][now.y][now.x]+1-dist[now.dir][y][x];
+                        d-=dist[now.dir][now.y][now.x]+1;
+                        d%=loop_dist;
+                        now=Point(y,x,now.dir);
+                        break;
+                    }
+                }else{
+                    // 範囲外に出た場合
+                    ll rest=d-dist[now.dir][now.y][now.x];
+                    //printf("2:");
+                    printf("%lld %lld\n",now.x+(ll)dx[now.dir]*rest,now.y+(ll)dy[now.dir]*rest);
+                    return ;
                 }
-                q.push({t.x+dx[(t.w+1)%4],t.y+dy[(t.w+1)%4],(t.w+1)%4,t.d+1});
-            }else{
-                q.push({x,y,t.w,t.d+1});
             }
-        }
-        printf("%lld %lld\n",t.x,t.y);
+            //printf("d:%lld\n",d);
+            // 残りの移動量だけシミュレーション
+            while(d--){
+                //printf("(y,x)=(%d,%d) d=%lld\n",now.y,now.x,d);
+                y=now.y+dy[now.dir];
+                x=now.x+dx[now.dir];
+                while(dist[now.dir][y][x]==WALL){
+                    // 行き先が障害物の場合
+                    now.dir=(now.dir+1)%MAX_D;
+                    y=now.y+dy[now.dir];
+                    x=now.x+dx[now.dir];
+                }
+                now=Point(y,x,now.dir);
+            }
+            //printf("3:");
+            printf("%d %d\n",now.x,now.y);
+        };
+
+        bfs(b,a);
     }
     return 0;
 }
